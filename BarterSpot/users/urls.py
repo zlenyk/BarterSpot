4e477cd8431a95917be756069edbefef5e99d355
jakubbrzeski <jk.brzeski@gmail.com>
2013-11-04 00:29:42 +0100

    Small fixes applied to registration.

--- BarterSpot/users/urls.py
+++ BarterSpot/users/urls.py
@@ -5,6 +5,7 @@ urlpatterns = patterns('',
 	url(r'^login/', views.login_view, name='login_view'),
 	url(r'^auth/', views.auth_user, name='auth_user'),
 	url(r'^register/', views.register_view, name='register_view'),
-	url(r'^add/', views.add_user, name='add_user')
+	url(r'^add/', views.add_user, name='add_user'),
+	url(r'^logout/', views.logout_user, name='logout_user'),
 )
 
--- BarterSpot/users/views.py
+++ BarterSpot/users/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 from django.contrib import auth
 from django.shortcuts import render
 from models import Member
+
+
 def register_view(request):
     return render(request, 'users/register.html', None)
 
@@ -12,7 +14,6 @@ def add_user(request):
     passw = request.POST.get('password')
     email = request.POST.get('email')
     User.objects.create_user(uname,email,passw)
-    
     firstName = request.POST.get('first_name')
     lastName = request.POST.get('last_name')
     _city = request.POST.get('city')
@@ -24,16 +25,23 @@ def login_view(request):
     return render(request, 'users/login.html', None)
 
 def auth_user(request):
-    username = request.POST.get('username', '')
-    password = request.POST.get('password', '')
+    username = request.POST.get('username')
+    password = request.POST.get('password')
     user = auth.authenticate(username=username, password=password)
+    import sys
+    print >> sys.stderr, 'spam'
     if user is not None and user.is_active:
         # Correct password, and the user is marked "active"
         auth.login(request, user)
         # Redirect to a success page.
-        context = None
+        context = { auth: True }
     	return render(request, 'base.html', context)
     else:
         # Show an error page
         context = None 
     	return render(request, 'base.html', context)
+
+def logout_user(request):
+	logout(request)
+	return render(request, 'base.html', context)
+
